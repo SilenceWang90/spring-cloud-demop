ea2ee124c01fa7d9355ba1137f32eb06c97deff6
Wangpeng@90 <wangpeng0717@sina.cn>
2023-02-28 15:03:59 +0800

    创建eureka-client项目，并初始化启动类

--- eureka/eureka-client/src/main/java/com/wp/springcloud/EurekaClientApplication.java
+++ eureka/eureka-client/src/main/java/com/wp/springcloud/EurekaClientApplication.java
@@ -1,10 +1,21 @@
 package com.wp.springcloud;
 
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @Description
  * @Author admin
  * @Date 2023/2/28 14:56
  */
-
+@SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaClientApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaClientApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
 }
