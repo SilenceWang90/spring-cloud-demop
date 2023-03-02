cc7dee199777620926e487741dd5f78abc79b8c0
Wangpeng@90 <wangpeng0717@sina.cn>
2023-03-02 16:48:30 +0800

    创建eureka-consumer项目，并初始化配置文件

--- eureka/eureka-consumer/src/main/java/com/wp/springcloud/EurekaConsumerApplication.java
+++ eureka/eureka-consumer/src/main/java/com/wp/springcloud/EurekaConsumerApplication.java
@@ -1,12 +1,21 @@
 package com.wp.springcloud;
 
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @Description
  * @Author admin
  * @Date 2023/3/2 16:38
  */
+@SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaConsumerApplication {
     public static void main(String[] args) {
-
+        new SpringApplicationBuilder(EurekaConsumerApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
     }
 }
