16a3a5775d47e413d9ade9493210780d2293a6a3
Wangpeng@90 <wangpeng0717@sina.cn>
2023-03-02 16:39:25 +0800

    创建eureka-consumer项目，并初始化配置文件

--- eureka/eureka-consumer/src/main/java/com/wp/springcloud/EurekaConsumerApplication.java
+++ eureka/eureka-consumer/src/main/java/com/wp/springcloud/EurekaConsumerApplication.java
@@ -3,7 +3,10 @@ package com.wp.springcloud;
 /**
  * @Description
  * @Author admin
- * @Date 2023/3/2 16:30
+ * @Date 2023/3/2 16:38
  */
 public class EurekaConsumerApplication {
+    public static void main(String[] args) {
+
+    }
 }
