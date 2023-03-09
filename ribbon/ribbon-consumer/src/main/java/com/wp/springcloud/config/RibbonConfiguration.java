c1d1d9b334d67c291c64988e1b99745e259e2139
Wangpeng@90 <wangpeng0717@sina.cn>
2023-03-09 18:10:08 +0800

    创建基于ribbon的消费端：负载均衡策略配置

---
+++ ribbon/ribbon-consumer/src/main/java/com/wp/springcloud/config/RibbonConfiguration.java
@@ -0,0 +1,19 @@
+package com.wp.springcloud.config;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description Ribbon配置类
+ * @Author admin
+ * @Date 2023/3/9 18:00
+ */
+@Configuration
+public class RibbonConfiguration {
+    @Bean
+    public IRule defaultLBStrategy() {
+        return new RandomRule();
+    }
+}
