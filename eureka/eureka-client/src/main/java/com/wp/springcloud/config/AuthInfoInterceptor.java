11b953c29eb437829f1d93c2515673d0bf342873
Wangpeng@90 <wangpeng0717@sina.cn>
2023-03-19 20:45:51 +0800

    基于Ribbin的超时配置

---
+++ eureka/eureka-client/src/main/java/com/wp/springcloud/config/AuthInfoInterceptor.java
@@ -0,0 +1,31 @@
+package com.wp.springcloud.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Description
+ * @Author admin
+ * @Date 2023/3/19 20:35
+ */
+@Configuration
+@Slf4j
+public class AuthInfoInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            log.info("cookie的名称{}", cookie.getName());
+            log.info("cookie的值{}", cookie.getValue());
+        }
+        String header1 = request.getHeader("header1");
+        String header2 = request.getHeader("header2");
+        log.info("获取header1为：{}，header2为：{}", header1, header2);
+        return false;
+    }
+}
---
+++ feign/feign-consumer/src/main/java/com/wp/springcloud/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.wp.springcloud.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description
+ * @Author admin
+ * @Date 2023/3/19 20:36
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private AuthInfoInterceptor authInfoInterceptor;
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInfoInterceptor).addPathPatterns("/**");
+        ;
+    }
+}
