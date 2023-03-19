2493f7a363d07825ca625c2ca2957c5e431a8f40
Wangpeng@90 <wangpeng0717@sina.cn>
2023-03-19 20:54:42 +0800

    基于Ribbin的超时配置

--- feign/feign-consumer/src/main/java/com/wp/springcloud/config/AuthInfoInterceptor.java
+++ feign/feign-consumer/src/main/java/com/wp/springcloud/config/AuthInfoInterceptor.java
@@ -1,8 +1,10 @@
 package com.wp.springcloud.config;
 
+import com.wp.springcloud.entity.Friend;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -28,4 +30,15 @@ public class AuthInfoInterceptor implements HandlerInterceptor {
         log.info("获取header1为：{}，header2为：{}", header1, header2);
         return false;
     }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        Friend friend = new Friend();
+        friend.setName("我是cookie");
+        friend.setPort("我是cookie的Port");
+        Cookie cookie = new Cookie("特制", friend.toString());
+        response.addCookie(cookie);
+    }
+
+
 }
