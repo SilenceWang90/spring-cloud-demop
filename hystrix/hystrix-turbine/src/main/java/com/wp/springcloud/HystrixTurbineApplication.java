4201c27bba164aed5cafd9c764b9d98a34b3d91b
Wangpeng@90 <wangpeng0717@sina.cn>
2023-07-21 12:04:49 +0800

    Hystrix组件:hystrix-turbine

---
+++ hystrix/hystrix-turbine/src/main/java/com/wp/springcloud/HystrixTurbineApplication.java
@@ -0,0 +1,26 @@
+package com.wp.springcloud;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * @Description
+ * @Author admin
+ * @Date 2023/7/21 12:01
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableTurbine
+public class HystrixTurbineApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixTurbineApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
