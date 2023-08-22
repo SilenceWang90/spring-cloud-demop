bb458aa8c9c7823104da0747721d9d54fa7e5124
Wangpeng@90 <wangpeng0717@sina.cn>
2023-08-22 14:29:52 +0800

    Config组件:config

---
+++ config/config-server/src/main/java/com/wp/springcloud/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.wp.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Description
+ * @Author admin
+ * @Date 2023/8/22 14:03
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
