7574159326c42218dd19ce77e221b569c8e595fb
Wangpeng@90 <wangpeng0717@sina.cn>
2023-09-07 11:03:12 +0800

    Gateway组件

---
+++ gateway/gateway-sample/src/main/java/com/wp/springcloud/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.wp.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description
+ * @Author admin
+ * @Date 2023/9/7 10:39
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
