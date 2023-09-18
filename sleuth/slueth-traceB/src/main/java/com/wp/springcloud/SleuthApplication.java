4c8939fee584e09b62b08985c11aff2751c923fe
Wangpeng@90 <wangpeng0717@sina.cn>
2023-09-18 12:41:51 +0800

    Sleuth组件

---
+++ sleuth/slueth-traceB/src/main/java/com/wp/springcloud/SleuthApplication.java
@@ -0,0 +1,18 @@
+package com.wp.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description
+ * @Author admin
+ * @Date 2023/9/18 12:41
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SleuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SleuthApplication.class, args);
+    }
+}
